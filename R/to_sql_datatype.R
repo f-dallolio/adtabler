15926500c2129d2477322dffb544a62aca71cf84
Filippo DallOlio <fdallolio@brocku.ca>
2023-10-16 23:01:18 -0400

    .

---
+++ R/to_sql_datatype.R
@@ -0,0 +1,30 @@
+#' Create sql name - datatype
+#'
+#' @param name character vector.
+#' @param type character vector.
+#' @param precision integer vector.
+#' @param scale integer vector.
+#' @param adintel logical. Default is TRUE.
+#'
+#' @return a character vector.
+#' @export
+to_sql_datatype <- function(name, type, precision = NA, scale = NA, adintel = TRUE) {
+  name_new <- adtabler::rename_adintel(name)
+  type_new <- adtabler::adintel_to_sql(type) |>
+    stringr::str_to_upper()
+  if (adintel) {
+    name <- name_new
+    type <- type_new
+  }
+  if((is.na(precision) & is.na(scale)) |
+     type_new == "DATE" |
+     type_new == "TEXT"){
+    out <- glue::glue("{name} {type}")
+  } else if (!is.na(precision) && is.na(scale)) {
+    out <- glue::glue("{name} {type}({precision})")
+  } else {
+    out <- glue::glue("{name} {type}({precision}, {scale})")
+  }
+  return(out)
+}
+to_sql_datatype <- Vectorize(to_sql_datatype)
--- scripts/temp/occurrences.R
+++ scripts/temp/occurrences.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(readxl)
 devtools::install_github('f-dallolio/fdutils')
 1
 library(fdutils)
@@ -6,17 +7,10 @@ devtools::install_github('f-dallolio/adtabler')
 1
 library(adtabler)
 
-to_c <- function(...){
-  out <- rlang::enquos(..., .named = TRUE)
-  names(out)
-}
-to_c(a,b,c)
-
-
-
 
 adintel_dir <- "/mnt/sata_data_1/new_adintel/"
 
+# adintel_files ----
 adintel_files <- list.files(
   path = adintel_dir,
   recursive = T
@@ -30,7 +24,33 @@ adintel_files <- list.files(
   ) |>
   nest(.by = file_type)
 
-occurrences_info <- adintel_files |>
+# unique_key_tables ----
+
+unique_key_table <- read_excel("Dropbox/NielsenData/unique_key_table.xlsx") |>
+  mutate(UniqueKey = UniqueKey |>
+           str_replace_all("MediatType", "MediaType") |>
+           str_remove_all(" ")) |>
+  rename_with(rename_adintel) |>
+  select(media_type_id, occurrence_filename, unique_key) |>
+  rename(file_name = occurrence_filename) |>
+  mutate(
+    file_name = file_name |>
+      str_remove_all(".tsv") |>
+      rename_adintel() |>
+      str_replace_all("national_", "network_"),
+    unique_key = unique_key |>
+      map(
+        ~ .x |>
+          str_split(",") |>
+          list_c() |>
+          rename_adintel() |>
+          as_tibble_col(column_name = "unique_key")
+
+      )
+    )
+
+# occurrences_temp ----
+occurrences_temp <- adintel_files |>
   filter(file_type == "occurrences") |>
   pluck("data", 1) |>
   mutate(temp = file,
@@ -44,8 +64,8 @@ occurrences_info <- adintel_files |>
       media_geo,
       media_type,
       temp
-      )
-    ) |>
+    )
+  ) |>
   mutate(
     media_type_id = temp |>
       str_remove_all("ok__") |>
@@ -53,16 +73,78 @@ occurrences_info <- adintel_files |>
       str_remove_all("id") |>
       as.integer(),
     .keep = "unused"
-  )
-occurrences_info
-
-adintel_info |>
-  filter(file_type == 'occurrences') |>
-  pull(media_info) |>
-  map(~ .x |> pluck(1)) |>
-  map(~ .x |> as_tibble()) |>
-  list_rbind()
+  ) |>
+  select(-file_type)
+#----
 
 
-|>
-  map(as_tibble)
+
+
+x <- adintel_tables |>
+  filter(file_type == "occurrences") |>
+  pull(var_attributes) |>
+  map(as_tibble) |>
+  list_rbind() |>
+  select(file_name, var_name_manual : scale) |>
+  mutate(
+    scale = if_else(var_name_manual == "spend", 2, scale),
+    col_names = rename_adintel(var_name_manual),
+    var_type_manual = if_else(str_detect(col_names, "ad_date"), "date", var_type_manual),
+    col_classes = adintel_to_sql(var_type_manual, r_out = TRUE),
+    sql_datatype = var_name_manual |>
+      to_sql_datatype(type = var_type_manual,
+                      precision = precision,
+                      scale = scale),
+    .before = precision
+  ) |>
+  select(- contains("var")) |>
+  nest(.by = file_name, .key = "col_info") |>
+  left_join(unique_key_table)
+
+final <- occurrences_temp |>
+  nest(file = file) |>
+  left_join(x) |>
+  select(-file_name) |>
+  relocate(media_type_id, .before = 1)
+
+
+
+file_df <- final |>
+  select(media_type_id : media_type, file) |>
+  unnest(everything())
+file_df
+
+
+
+
+
+
+
+col_df <- final |>
+  select(media_type_id, col_info) |>
+  unnest(everything()) |>
+  select(media_type_id : col_classes, sql_datatype)
+col_df
+ukey_df <- final |>
+  select(media_type_id, unique_key) |>
+  unnest(everything())
+ukey_df
+#
+# library(dm)
+#
+# my_dm <- dm(file_df, col_df, ukey_df)
+# my_dm <- my_dm |>
+#   dm_add_pk(table = file_df, columns = media_type_id) |>
+#   dm_add_pk(table = col_df, columns = c(media_type_id, col_names)) |>
+#   dm_add_pk(table = ukey_df, columns = c(media_type_id, unique_key)) |>
+#   dm_add_fk(table = file_df, columns = media_type_id, ref_table = col_df) |>
+#   dm_add_fk(table = file_df, columns = media_type_id, ref_table = ukey_df)
+# dm_draw(my_dm)
+#
+# dm_flatten_to_tbl(dm = my_dm, .start = file_df)
+# dm_examine_constraints(my_dm)
+#
+#
+#
+#
+
