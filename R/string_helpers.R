7a3ff17876f2c46ad4c9d579e68d17f2541d5a9d
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-10-28 07:06:08 -0400

    .

--- R/string_helpers.R
+++ R/string_helpers.R
@@ -12,3 +12,47 @@ NULL
 no_case <- function(pattern) {
   stringr::regex(pattern, ignore_case = TRUE)
 }
+
+#' @rdname string_helpers
+#' @export
+is_upper <- function(x, which = FALSE) {
+  stopifnot(is.character(x))
+  if (which) {
+    return(stringr::str_which(x, "^[:upper:]+$"))
+  }
+  stringr::str_detect(x, "^[:upper:]+$")
+}
+
+#' @rdname string_helpers
+#' @export
+is_lower <- function(x, which = FALSE) {
+  stopifnot(is.character(x))
+  if (which) {
+    return(stringr::str_which(x, "^[:lower:]+$"))
+  }
+  stringr::str_detect(x, "^[:lower:]+$")
+}
+
+#' @rdname string_helpers
+#' @export
+str_upper_uscore <- function(x, to_lower = TRUE){
+  x <- unlist(strsplit(x, ""))
+  id <- which(is_upper(x))
+  x[id[id!=1]] <- paste0("_", x[id[id!=1]])
+  out <- paste0(x, collapse = "")
+  if( to_lower ) return(tolower(out))
+  out
+}
+
+#' @rdname string_helpers
+#' @export
+rename_adintel <- function(x, named = TRUE){
+  nms <- x
+  out <- str_upper_uscore(x, to_lower = TRUE) |>
+    str_replace_all("prime", "prim")
+  out[str_detect(out, "dim_bridge")] <- "dim_bridge_occ_imp_spot_radio_key"
+  if( named ){
+    names(out) <- nms
+  }
+  out
+}
