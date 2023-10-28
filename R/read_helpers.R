b83d1cef35292fbbb75bff64607b4b3020c013d6
Filippo DallOlio <fdallolio@brocku.ca>
2023-10-28 17:50:55 -0400

    .

--- R/read_helpers.R
+++ R/read_helpers.R
@@ -10,23 +10,17 @@ NULL
 
 #' @rdname read_files
 #' @export
-read_nrows <- function(file, header = TRUE){
-  has_space <- stringr::str_detect(file, " ")
-  if ( has_space ){
-    old_file_name <- file
-    new_file_name <- stringr::str_replace_all(file, " ", "_")
-    file.rename(from = file, to = new_file_name)
-  }
-  cmd_out <- sprintf("wc -l %s", new_file_name) |>
-    system(intern = TRUE) |>
-    strsplit(" ") |>
-    unlist() |>
-    as.numeric() |>
-    suppressWarnings()
-  if( has_space ) {
-    file.rename(new_file_name, old_file_name)
-  }
-  cmd_out[!is.na(cmd_out)] - header
+read_nrows <- function(file){
+  cmd_out <- sprintf("wc -l %s", file) |>
+    system(intern = TRUE)
+  # |>
+  #   strsplit(" ")
+  # |>
+  #   unlist() |>
+  #   as.numeric() |>
+  #   suppressWarnings()
+  cmd_out
+  # cmd_out[!is.na(cmd_out)] - header
 }
 
 
