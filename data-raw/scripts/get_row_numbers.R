647b5025f91068fb0600d5b16e401c14c0fd10e2
Filippo DallOlio <fdallolio@brocku.ca>
2023-10-28 22:55:02 -0400

    .

--- data-raw/scripts/get_row_numbers.R
+++ data-raw/scripts/get_row_numbers.R
@@ -18,8 +18,8 @@ get_row_numbers <- out |>
   stringr::str_split(" ", simplify = TRUE) |>
   `colnames<-`(c("n_rows", "full_file_name")) |>
   tibble::as_tibble() |>
-  mutate(n_rows_no_header = as_numeric2(n_rows),
-         n_rows_w_header = n_rows_no_header - 1) |>
+  mutate(n_lines = as_numeric2(n_rows),
+         n_rows = n_rows_no_header - 1) |>
   select(-n_rows)
 
 usethis::use_data(get_row_numbers, overwrite = TRUE)
