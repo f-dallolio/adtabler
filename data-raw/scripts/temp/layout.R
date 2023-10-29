79f4518fae34676bf3febbd5128c35d4c887bbd2
Filippo DallOlio <fdallolio@brocku.ca>
2023-10-29 17:11:34 -0400

    .

--- data-raw/scripts/temp/layout.R
+++ data-raw/scripts/temp/layout.R
@@ -2,17 +2,16 @@ library(tidyverse)
 library(adtabler)
 
 data("layout_list")
-data("adintel_files")
+# data("adintel_files")
 
 layout_list
+adintel_files
 
-x <- adintel_files |>
+adintel_files |>
   unnest(everything()) |>
-  filter(str_detect(file_name, "lock", negate = T)) |>
   nest(.by = c(contains("_std"), col_pos)) |>
   distinct() |>
-  full_join(layout_list |>
-              mutate(file_type_std = if_else(file_name_std == "ue_market_breaks", "universe_estimates", file_type_std))) |>
+  full_join(layout_list) |>
   arrange(row) |>
   unnest(everything())
 
