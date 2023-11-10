3cf6d573279ae29098a1f38030a66707ea6f9d83
Filippo DallOlio <fdallolio@brocku.ca>
2023-11-09 19:30:07 -0500

    .

---
+++ data-raw/data_write_to_db.R
@@ -0,0 +1,21 @@
+devtools::install_github("f-dallolio/adtabler")
+1
+library(data.table)
+library(tidyverse)
+library(rlang)
+library(glue)
+library(adtabler)
+library(prettyunits)
+
+data_write_to_db <- data_info_db |>
+  # filter(year |> not_na()) |>
+  dplyr::transmute(file = full_file_name,
+            year,
+            tbl_name = db_name_std,
+            col_names = col_info |> purrr::map(~ .x$col_name_std |> unname()),
+            col_classes = col_info |> purrr::map(~ .x$datatype_r |> unname()),
+            col_uk = col_info |> purrr::map(~ .x$is_uk |> unname()),
+            overwrite = is.na(overwrite),
+            append = !overwrite)
+
+usethis::use_data(data_write_to_db, overwrite = TRUE)
--- write_to_db.R
+++ write_to_db.R
@@ -13,16 +13,16 @@ library(prettyunits)
 library(DBI)
 library(RPostgres)
 
-xxx <- data_info_db |>
-  filter(year |> is.na()) |>
-  transmute(file = full_file_name,
-            year,
-            tbl_name = db_name_std,
-            col_names = col_info |> map(~ .x$col_name_std |> unname()),
-            col_classes = col_info |> map(~ .x$datatype_r |> unname()),
-            col_uk = col_info |> map(~ .x$is_uk |> unname()),
-            overwrite = is.na(overwrite),
-            append = !overwrite)
+# data_write_to_db <- data_info_db |>
+#   filter(year |> not_na()) |>
+#   transmute(file = full_file_name,
+#             year,
+#             tbl_name = db_name_std,
+#             col_names = col_info |> map(~ .x$col_name_std |> unname()),
+#             col_classes = col_info |> map(~ .x$datatype_r |> unname()),
+#             col_uk = col_info |> map(~ .x$is_uk |> unname()),
+#             overwrite = is.na(overwrite),
+#             append = !overwrite)
 
 dbname <- 'adintel'
 host <- '10.147.18.200'
@@ -36,57 +36,110 @@ con <- dbConnect(
   password = rstudioapi::askForPassword()
 )
 
-file = xxx$file[[1]]
-year = xxx$year[[1]]
-tbl_name = xxx$tbl_name[[1]]
-col_names = xxx$col_names[[1]]
-col_classes = xxx$col_classes[[1]]
+file = data_write_to_db$file[[1]]
+year = data_write_to_db$year[[1]]
+media_type_id = data_write_to_db$media_type_id[[1]]
+tbl_name = data_write_to_db$tbl_name[[1]]
+col_names = data_write_to_db$col_names[[1]]
+col_classes = data_write_to_db$col_classes[[1]]
 col_classes[col_names == "ad_date"] <- "IDate"
-col_uk = xxx$col_uk[[1]]
-overwrite = xxx$overwrite[[1]]
-append = xxx$append[[1]]
+col_uk = data_write_to_db$col_uk[[1]]
+overwrite = data_write_to_db$overwrite[[1]]
+append = data_write_to_db$append[[1]]
 
-xx_list <- as.list(xxx)
 
 write_to_db <- function(con = {{con}}, file, year, tbl_name, col_names, col_classes, col_uk, overwrite, append){
 
-  read_info <- str_flatten(c(tbl_name, year), collapse = " - ", na.rm = TRUE)
-  print(glue::glue("Starting: \t {read_info}"))
+  read_info <- stringr::str_flatten(c(tbl_name, year), collapse = " - ", na.rm = TRUE)
+  print(glue::glue("Starting: \t\t {read_info}"))
 
   t0_fun <- Sys.time()
 
   has_ad_time <- "ad_time" %in% col_names
 
-  is_ref_dyn <- str_detect(tbl_name, "ref_dyn__")
-  is_ref <- str_detect(tbl_name, "ref__")
+  is_ref_dyn <- stringr::str_detect(tbl_name, "ref_dyn__")
+  is_ref <- stringr::str_detect(tbl_name, "ref__")
 
   col_uk <- col_names[col_uk]
   col_classes[col_names == "ad_date"] <- "IDate"
 
   t0_fread <- Sys.time()
-  df <- fread(file = file, colClasses = col_classes, col.names = col_names, key = col_uk, nThread = parallel::detectCores() - 2, encoding = "UTF-8")
-  elapsed(t0_fread, msg = 'File read in \t {.x}') |> print()
+
+  if(tbl_name == "ref_dyn__brand" & year %in% 2018:2021){
+    tbl_tmp <- data.table::fread(
+      file = file,
+      sep = "",
+      quote = ""
+    ) |>
+      dplyr::pull(1) |>
+      stringr::str_replace_all("\t\"", "\"")
+    df <- data.table::fread(
+      text = tbl_tmp,
+      colClasses = col_classes,
+      col.names = col_names,
+      key = col_uk,
+      nThread = parallel::detectCores() - 2,
+      encoding = "UTF-8"
+    )
+  } else
+    if(tbl_name == "ref_dyn__distributor" & year %in% 2018){
+      tbl_tmp <- data.table::fread(
+        file = file,
+        sep = "",
+        quote = ""
+      )
+      tmp1 <- tbl_tmp |> dplyr::slice(1:22035)
+      tmp2 <- tbl_tmp |> dplyr::slice(22036:22038) |> dplyr::pull(1) |> stringr::str_flatten()
+      tmp3 <- tbl_tmp |> dplyr::slice(22039:NROW(tbl_tmp)) |> dplyr::pull(1)
+      tmp_new <- c(names(tbl_tmp), tmp1, tmp2, tmp3) |> list_c()
+      df <- data.table::fread(
+        text = tmp_new,
+        colClasses = col_classes,
+        col.names = col_names,
+        key = col_uk,
+        nThread = parallel::detectCores() - 2,
+        encoding = "UTF-8"
+      )
+    } else
+    {
+      df <- data.table::fread(
+        file = file,
+        colClasses = col_classes,
+        col.names = col_names,
+        key = col_uk,
+        nThread = parallel::detectCores() - 2,
+        encoding = "UTF-8"
+      )
+    }
+
+  timer(t0_fread, msg = 'File read in \t {.x}') |> print()
 
   if(has_ad_time){
-    df$ad_time <- str_sub(df$ad_time, 1, 8)
+    df$ad_time <- stringr::str_sub(df$ad_time, 1, 8)
   }
 
   if(is_ref_dyn){
-    df  <- df |> mutate(year = year, .before = 1)
+    df  <- df |> dplyr::mutate(year = year, .before = 1)
   }
 
   t0_dbwrite <- Sys.time()
+
   RPostgres::dbWriteTable(conn = con,
                           name = tbl_name,
                           value = df,
                           overwrite = overwrite,
                           append = append)
-  elapsed(t0_dbwrite, n_post = 2, msg = 'Table to DB in \t {.x}') |> print()
 
+  timer(t0_dbwrite, msg = 'Table to DB in \t {.x}') |> print()
+
+  timer(t0_fun, n_post = 2, msg = 'Done in \t {.x} \t {read_info}') |> print()
 }
 
-p_write_to_db <- partial(.f = write_to_db, con = con)
+data_write_to_db_list <- as.list(data_write_to_db)
 
-pmap(xx_list, p_write_to_db)
+write_to_db_con <- function(con){
+  purrr::partial(.f = write_to_db, con = con)
+}
+pwalk(data_write_to_db_list, p_write_to_db)
 
 dbDisconnect(con)
