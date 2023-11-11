387ff1ad6504a1b38bb315af4afa93c91ad88f66
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-11-10 19:25:26 -0500

    ..

--- R/vector_slice.R
+++ R/vector_slice.R
@@ -4,14 +4,48 @@
 #' @param i integer or integer vector. Negative integers indicate positions from the end.
 #'
 #' @return a vector of the same kind of x.
+#'
+#' @name vector_slice
+NULL
+
+#' @rdname vector_slice
 #' @export
 #'
-slice_vec <- function(x, i){
+slice_vec <- function(x, i, negate = FALSE){
   stopifnot( "abs(i) should be an integer in 1, ... , vec_size(x)" =
                all(abs(i) |> dplyr::between(1, vctrs::vec_size(x))))
   ii <- dplyr::case_when(
     i > 0 ~ i,
     i < 0 ~ vctrs::vec_size(x) + i + 1
   )
-  x[ii]
+  out <- x[ii]
+  if(negate){
+    return(setdiff(x, out))
+  }
+  out
+}
+
+#' @rdname vector_slice
+#' @export
+#'
+slice_first <- function(x, n, negate = FALSE){
+  i <- seq.int(from = 1,to = n)
+  out <- x[i]
+  if(negate){
+    return(setdiff(x, out))
+  }
+  out
+}
+
+#' @rdname vector_slice
+#' @export
+#'
+slice_last <- function(x, n, negate = FALSE){
+  id = sort(seq_along(x),decreasing = TRUE)
+  i <- sort(id[seq.int(from = 1,to = n)])
+  out <- x[i]
+  if(negate){
+    return(setdiff(x, out))
+  }
+  out
 }
