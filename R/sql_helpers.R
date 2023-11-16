1ac9f63de06495016e42b108d6cb74c31030e702
Filippo DallOlio <fdallolio@brocku.ca>
2023-11-16 09:00:34 -0500

    .

--- R/sql_helpers.R
+++ R/sql_helpers.R
@@ -1,22 +1,53 @@
-sql_coldef <- function(.name, .type, .d1, .d2, .con){
-  n = length(.name)
-  comma <- c( rep(",", n-1), "" )
-  out <- dplyr::case_when(
-    !is.na(.d1) & !is.na(.d2) ~ glue::glue("{.name} {.type}({ .d1 }, { .d2 }) {.con}",.na = ""),
-    !is.na(.d1) & is.na(.d2) ~ glue::glue("{.name} {.type}({ .d1 }) {.con}",.na = ""),
-    .default = glue::glue("{.name} {.type} {.con}",.na = "")
-  )
-  stringr::str_squish(out) |> str_flatten_comma()
-  # glue::glue("\t{out}{comma}")
+#' SQL column definition block
+#'
+#' @param .col_name character.
+#' @param .col_type character.
+#' @param .n1 integer. Defaults to NA.
+#' @param .n2 integer. Defaults to NA.
+#'
+#' @return glue string.
+#'
+#' @name sql_helpers
+NULL
+
+#' @rdname sql_helpers
+#' @export
+#'
+sql_define_cols <- function(.col_name, .col_type, .n1 = NA, .n2 = NA, ...) {
+  df <- tibble::tibble(
+    .col_name = paste0(
+      stringr::str_pad(
+        string = .col_name,
+        width = max(nchar(.col_name)),
+        side = "right",
+        pad = " ")
+    ),
+    .col_type = .col_type,
+    .n1 = .n1,
+    .n2 = .n2,
+  ) |>
+    dplyr::mutate(
+      coldef = dplyr::if_else(
+        .col_type == "NUMERIC",
+        glue::glue('    {.col_name} { .col_type }({.n1}, {.n2})'),
+        glue::glue('    {.col_name} { .col_type }')
+      )
+    )
+
+  df$col_def |>
+    glue::glue_collapse(sep = ", \n")
 }
 
-sql_coldef_from_tbl <- function(x){
-  x <-  attributes(x)
-  .n <- x$names
-  .t <- x$sql_type
-  .l <- x$sql_length
-  .s <- x$sql_scale
-  .c <- x$sql_constraints
-  sql_coldef(.n, .t, .l, .s, .c)
+#' @rdname sql_helpers
+#' @export
+#'
+sql_create_tbl <- function(tbl_name, col_def){
+  glue::glue(
+    'CREATE TABLE IF NOT EXISTS { tbl_name} \n ( \n { xx } \n )'
+  )
 }
+
+
+
+
 
