828dadec82d1d2993f3a5ad0bece40216842b866
Filippo DallOlio <fdallolio@brocku.ca>
2023-11-19 19:23:13 -0500

    .

--- R/vector_slice.R
+++ R/vector_slice.R
@@ -11,16 +11,22 @@ NULL
 #' @rdname vector_slice
 #' @export
 #'
-slice_vec <- function(x, i, negate = FALSE){
+slice_vec <- function(x, i, sort = TRUE, negate = FALSE, simplify = FALSE){
   stopifnot( "abs(i) should be an integer in 1, ... , vec_size(x)" =
                all(abs(i) |> dplyr::between(1, vctrs::vec_size(x))))
   ii <- dplyr::case_when(
     i > 0 ~ i,
     i < 0 ~ vctrs::vec_size(x) + i + 1
   )
+  if (sort) {
+    ii <- sort(ii)
+  }
   out <- x[ii]
   if(negate){
-    return(setdiff(x, out))
+    out <- setdiff(x, out)
+  }
+  if (simplify) {
+    out <- unlist(out, recursive = FALSE, use.names = TRUE)
   }
   out
 }
@@ -28,11 +34,14 @@ slice_vec <- function(x, i, negate = FALSE){
 #' @rdname vector_slice
 #' @export
 #'
-slice_first <- function(x, n, negate = FALSE){
+slice_first <- function(x, n = 1, negate = FALSE, simplify = FALSE){
   i <- seq.int(from = 1,to = n)
   out <- x[i]
   if(negate){
-    return(setdiff(x, out))
+    out <- setdiff(x, out)
+  }
+  if (simplify) {
+    out <- unlist(out, recursive = FALSE, use.names = TRUE)
   }
   out
 }
@@ -40,12 +49,15 @@ slice_first <- function(x, n, negate = FALSE){
 #' @rdname vector_slice
 #' @export
 #'
-slice_last <- function(x, n, negate = FALSE){
+slice_last <- function(x, n, negate = FALSE, simplify = FALSE){
   id = sort(seq_along(x),decreasing = TRUE)
   i <- sort(id[seq.int(from = 1,to = n)])
   out <- x[i]
   if(negate){
-    return(setdiff(x, out))
+    out <- setdiff(x, out)
+  }
+  if (simplify) {
+    out <- unlist(out, recursive = FALSE, use.names = TRUE)
   }
   out
 }
