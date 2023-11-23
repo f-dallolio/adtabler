320350b1f1230e24ea0832d8928ca8625d7a41b2
Filippo DallOlio <fdallolio@brocku.ca>
2023-11-23 04:53:49 -0500

    .

--- R/fall_date.R
+++ R/fall_date.R
@@ -7,33 +7,17 @@
 #' @return A tibble or a list.
 #' @export
 #'
-fall_date <- function(year, yearweek = TRUE, out_tibble = TRUE){
-
-  fall_date <- fall_date1 <- rep(NA, length(year))
+fall_date <- function(year,yearweek = TRUE) {
+  fall_date <- rep(NA, length(year))
   no_na_id <- not_na(year)
-
   fall_date_0 <- fmdates:::equinox(year[no_na_id], season = 'sep') |>
-    as.Date()
-  fall_date1_0 <- fmdates:::equinox(year[no_na_id] + 1, season = 'sep') |>
     as.Date()
   if( yearweek ){
     fall_date_0 <- fall_date_0 |>
       tsibble::yearweek() |> as.Date()
-    fall_date1_0 <- fall_date1_0 |>
-      tsibble::yearweek() |> as.Date()
   }
   fall_date[no_na_id] <- as.character(fall_date_0)
-  fall_date1[no_na_id] <- as.character(fall_date1_0)
-
-  out <- dplyr::tibble(
-    year = year,
-    fall_date = fall_date,
-    fall_date_p1year = fall_date1
-  )
-  if( !out_tibble ){
-    out <- as.list(out)
-  }
-  out
+  fall_date
 }
 
 
