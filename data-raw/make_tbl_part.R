bb312f2863fcc20c15fc3c96387e4784bb146dd4
Filippo DallOlio <fdallolio@brocku.ca>
2023-11-23 20:08:51 -0500

    .

--- data-raw/make_tbl_part.R
+++ data-raw/make_tbl_part.R
@@ -17,15 +17,20 @@ con <- dbConnect(RPostgres::Postgres(),
                  user = 'postgres',
                  password = '100%Postgres')
 
-tbl_info_tot$file |>  unlist()
-tbl_info_tot
+# tbl_info_tot <- tbl_info_tot |> select(-date_from, -date_to) |>
+#   left_join(
+#     tbl_dates_from_to |>
+#       select(file_name_std, dynamic_flag, date_from, date_to) |>
+#       summarise(dynamic_flag = unique(dynamic_flag),
+#                 across(!dynamic_flag, list), .by = file_name_std)
+#   ) |>
+#   relocate(dynamic_flag, adintel_year, date_from, date_to, .before = file)
+# usethis::use_data(tbl_info_tot, overwrite = T)
 
 
-# |>fn_fmls_names(sql_build_tbl)[1:5] <- fn_fmls_names(sql_build_tbl)[1:5] |> str_remove_all('\\.')
 
-xxx <- unnest(tbl_info_tot, file)
-xxx$file |>
-  # str_subset(no_case('brand')) |>
+x <- tbl_info_tot$file |>
+  unlist() |>
   # slice_vec(1) |>
   map(
   ~ .x |> decompose_file() |>
@@ -37,51 +42,36 @@ xxx$file |>
       select(file_name_std, col_names_std : sql_datatype_min, pk)
   ) |>
     transmute(
+      file_name_std,
       .tbl_name = file_name_std,
       .col_names = col_names_std,
       .data_types = sql_datatype,
       .pk = pk,
-      .part_col = 'ad_date',
       .part_name = paste(.tbl_name, paste0("y", adintel_year), sep = "__"),
       .from = unlist(date_from),
       .to = unlist(date_to)
+    ) |>
+    mutate(
+      .part_col = if_else(is.na(.from), NA, 'ad_date'),
+      .before = .part_name
     )
   # |>   pmap(sql_build_tbl)
-) |> list_rbind()
-x <- .Last.value |>
-  mutate(.tbl_name2 = .tbl_name) |>
+) |>
+  list_rbind() |>
   nest(.by = !(c(.tbl_name, .part_name : .to))) |>
-  rename(.tbl_name = .tbl_name2) |>
-  relocate(.tbl_name, 1)
+  mutate(.tbl_name = file_name_std,
+         .after = file_name_std) |>
+  left_join(tbl_dates_from_to |> select(file_name_std, dynamic_flag))
+x
 
-
-
-x$tbl_code <- x|>
+tbl_code <- x |>
   pmap(sql_build_tbl) |> map(~.x) |> set_names(x$.tbl_name)
-x$part_code <- x$data |>
+x$tbl_code <- tbl_code
+
+part_code <- x |>
+  filter(dynamic_flag) |>
+  pull(data) |>
   map(~ .x |> pmap(sql_create_part) |> set_names(.x$.part_name))
-x |>  select(.tbl_name, tbl_code, part_code)
-tbl_info_tot |> print(n=200)
-tbl_info_tot$file[tbl_info_tot$file_name_std == 'media_type'] <- list('/mnt/sata_data_1/adintel/ADINTEL_DATA_2021/nielsen_extracts/AdIntel/Master_Files/Latest/Dates.tsv')
-usethis::use_data(tbl_info_tot, overwrite = T)
+x$part_code <- rep(NA, NROW(x)) |> as.list()
+x$part_code[(x$dynamic_flag)] <- part_code
 
-
-
-map_vec(2010:2021, ~ glue('/mnt/sata_data_1/adintel/ADINTEL_DATA_{ .x }/nielsen_extracts/AdIntel/Master_Files/Latest/Dates.tsv')) |>
-  walk(~ lookup_date |> write_tsv(.x))
-
-
-
-
-
-
-xx <- x |>
-
-  nest(.by = .tbl_name) |>
-  mutate(data = data |> map(~ as.list(.x) |> set_names(names(.x))))
-xx
-
-
-
-x$create_table_code <- map2(x$tbl_code, x$part_code, ~ c(list(.x), .y))
-x$create_table_code$.tbl_name
