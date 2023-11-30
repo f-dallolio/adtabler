a38cea5933b84ef5b7cbc8bcd4cb73156a37c7c1
Filippo Dall'Olio <fdallolio@brocku.ca>
2023-11-29 22:43:41 -0500

    .

--- data-raw/last.R
+++ data-raw/last.R
@@ -21,20 +21,26 @@ sqlr <- read_csv("~/Documents/sqlr.csv") |>
            specs = str_remove_all(specs0, "\\(") |> str_remove_all("\\)") |>
              str_remove_all('\\[') |>  str_remove_all('\\]') |> str_squish() |> map(str_split_comma)) |>
     unnest(specs) |>
-    mutate( class = if_else(specs %in% c('s', 'p', 'n'), "integer", "character"))
-  |>
+    mutate( class = if_else(specs %in% c('s', 'p', 'n'), "integer", "character")) |>
     nest(specs0 = (specs0),
-         data = is_optional : class, .by = c(id, type))
-  |>
+         data = is_optional : class, .by = c(id, type))  |>
     mutate(specs0 = specs0 |> map(~ .x |> pull(specs0) |> unique() |> str_flatten(collapse = " "))) |>
-    unnest(specs0) |>
+    unnest(everything()) |>
     mutate(specs1 = specs0 |>
              str_replace_all( "\\] \\[", " x/x ") |>
              str_replace_all( "\\]", " x/ ") |>
-             str_remove_all("\\]") |> str_remove_all("\\["))
-
-  nest(.by = c(id, type)) |>
-    mutate(data = data |> set_names(type)) |>
+             str_remove_all("\\]") |> str_remove_all("\\[")) |> nest(.by = c(id, type, specs0)) |>
+    mutate(data = data |> set_names(type),
+           specs1 = specs0 |>
+             str_replace_all("\\[", "\\{ \\.") |>
+             str_replace_all("\\]", "\\ }") |>
+             str_remove_all(" \\(") |>
+             str_remove_all("\\) ") |>
+             str_replace_all('\\. ', '\\.') |>
+             str_remove_all('\\, ') |>
+             str_replace_all('.without time zone', 'wo_tz') |>
+             str_squish()) |> view()
+  |>
     pull(data)
 
 
